b119adc55e1015de0a1b7355b6468befab0c5a9b
Elliot <elliotpurouski@gmail.com>
2020-08-28 18:48:59 +0300

    Добавлено решение задания "omit"

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,16 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const newObj = {};
+  const isPropContain = (fieldProp, objProp) => fieldProp === objProp;
 
+  for (let prop in obj) {
+    if (fields.some(isPropContain.bind(null, prop))) {
+      continue;
+    }
+
+    newObj[prop] = obj[prop];
+  }
+
+  return newObj;
 };
