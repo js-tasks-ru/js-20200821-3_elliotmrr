f245131840ba6524a03fdf9dbac020530fa00d93
Elliot <elliotpurouski@gmail.com>
2020-08-28 06:19:09 +0300

    Решение задачи "sortStrings"

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,29 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  compareStr = compareWithDirection(compareStr, param);
+  const copyArr = arr.slice();
 
+  return copyArr.sort(compareStr);
+
+  function compareWithDirection(f, direction) {
+    return function(a, b) {
+      return direction === "asc" ? f(a, b) : f(b, a);
+    };
+  }
+
+  function compareStr(a, b) {
+    const langReg = [
+      "en-GB",
+      "en-US",
+      "ru-RU",
+      "ru-BY",
+    ];
+    const collator = new Intl.Collator(langReg, {
+      sensitivity: "variant",
+      caseFirst: "upper",
+    });
+
+    return collator.compare(a, b);
+  }
 }
