3b7aa8e4a13ed05c610237fc934937d61cd9c5ea
Elliot <elliotpurouski@gmail.com>
2020-08-29 02:21:48 +0300

    Fix. Task "omit". Переписал решение - убрал "лишнее".

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -6,14 +6,11 @@
  */
 export const omit = (obj, ...fields) => {
   const newObj = {};
-  const isPropContain = (fieldProp, objProp) => fieldProp === objProp;
 
-  for (let [key, value] of Object.entries(obj)) {
-    if (fields.some(isPropContain.bind(null, key))) {
-      continue;
+  for (const [key, value] of Object.entries(obj)) {
+    if (!fields.includes(key)) {
+      newObj[key] = value;
     }
-
-    newObj[key] = value;
   }
 
   return newObj;
