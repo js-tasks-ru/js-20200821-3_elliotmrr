96ef2343ec1c1be67867304eb903b428bf9e8c09
Elliot <elliotpurouski@gmail.com>
2020-09-01 09:05:08 +0300

    Module 3. Task 3 - trimSymbols. Добавлено решение

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,40 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  switch (size) {
+  case 0:
+    return "";
 
+  case undefined:
+    return string;
+
+  default:
+    return trimmedStr();
+  }
+
+  function trimmedStr() {
+    const currentCountChar = new Map();
+    let count;
+    const filter = char => {
+      count = currentCountChar.get(char);
+
+      switch (true) {
+      case count === undefined:
+        currentCountChar.set(char, 1);
+        return true;
+
+      case count < size:
+        currentCountChar.set(char, ++count);
+        return true;
+
+      default:
+        return false;
+      }
+    };
+
+    return string
+      .split('')
+      .filter(filter)
+      .join('');
+  }
 }
