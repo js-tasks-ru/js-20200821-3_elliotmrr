cf9e07ca210c6f2b711c6f335d0cbd26d8363ee6
Elliot <elliotpurouski@gmail.com>
2020-09-06 19:30:12 +0300

    Module #4 | Task 1 - columnChart: fixes after review

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,77 +1,104 @@
 export default class ColumnChart {
   element; // HTMLElement;
+  subElements = {};
+  chartHeight = 50;
 
   constructor({
     data = [],
-    label = "noName",
+    label = "",
     value = 0,
-    link = "#"
+    link = "",
   } = {}) {
-    this.chartHeight = 50;
+    this.data = data;
     this.label = label;
     this.value = value;
     this.link = link;
-    this.data = data;
 
     this.render();
-    this.initEventListeners();
   }
 
-  render() {
-    this.template = `<div class="column-chart${(this.data.length === 0) ? " column-chart_loading" : ""}">
+  getColumnBody(data) {
+    const maxValue = Math.max(...data);
+    const scale = this.chartHeight / maxValue;
+
+    return data
+      .map(item => {
+        const percent = (item / maxValue * 100).toFixed(0);
+
+        return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+      })
+      .join('');
+  }
+
+  getLink() {
+    return this.link ? `<a class="column-chart__link" href="${this.link}">View all</a>` : '';
+  }
+
+  get template() {
+    return `
+      <div class="column-chart ${(this.data.length === 0) ? "column-chart_loading" : ""}"
+           style="--chart-height: ${this.chartHeight}"
+      >
         <div class="column-chart__title">
-          ${this.label}
-          <a class="column-chart__link" href="${this.link}">View all</a>
+          Total ${this.label}
+          ${this.getLink()}
         </div>
         <div class="column-chart__container">
           <div data-element="header" class="column-chart__header">
             ${this.value}
           </div>
           <div data-element="body" class="column-chart__chart">
-            ${this.dataParsingTag`${this.data}`}
+            ${this.getColumnBody(this.data)}
           </div>
         </div>
-      </div>`;
-
-    const element = document.createElement('div');
-    element.innerHTML = this.template;
-    this.element = element.firstElementChild;
+      </div>
+    `;
   }
 
-  update(data = []) {
-    if (!data.length) {
+  render() {
+    const element = document.createElement('div');
+
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+
+    this.subElements = this.getSubElements(this.element);
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
+  }
+
+  /*
+  * Обновление всего чарта не реализовано.
+  * При обновлении всего чарта, можно заносить все данные (data, label, value, link)
+  * в объект и рендерить весь чарт без проверки на пустые данные
+  * и добавлении класса для склетона,
+  * т.к проверка уже в get template(){} осуществляется, при формировании строки.
+  * */
+  update(bodyData = []) {
+    if (!bodyData.length) {
+      this.element.classList.add("column-chart_loading"); // Линк продолжает отображаться
+      this.subElements.body.innerHTML = "";
+
       return;
     }
 
-    this.data = data;
+    /*
+    * Оставим первоначальное значение в объекте, по аналогии с <input>.
+    * К тому же у нас название, значение и линк "старые".
+    * */
+    // this.data = bodyData;
+
+    this.subElements.body.innerHTML = this.getColumnBody(bodyData);
 
     this.element.classList.remove("column-chart_loading");
-    this.element.querySelector('[data-element="body"]')
-      .innerHTML = this.dataParsingTag`${this.data}`;
-  }
-
-  dataParsingTag(strings, ...args) {
-    const data = args[0];
-    const maxValue = Math.max(...data);
-    const scale = this.chartHeight / maxValue;
-    let result = ``;
-
-    for (const item of data) {
-      const percent = (item / maxValue * 100).toFixed(0);
-      const value = Math.floor(item * scale);
-
-      result += `<div style="--value: ${value}" data-tooltip="${percent}%"></div>`;
-    }
-
-    return result;
-  }
-
-  initEventListeners() {
-
-  }
-
-  removeEventListeners() {
-
   }
 
   remove () {
@@ -80,6 +107,6 @@ export default class ColumnChart {
 
   destroy() {
     this.remove();
-    this.removeEventListeners();
+    this.subElements = {};
   }
 }
